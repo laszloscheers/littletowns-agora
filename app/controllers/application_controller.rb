0ead089f63c1a2dfe7dbd04231db188ea05d0ee1
Laszlo-VSC <laszloluque@gmail.com>
2023-05-11 16:10:10 +0200

    links, controllers, and security threats fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,16 @@ class ApplicationController < ActionController::Base
         devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
         devise_parameter_sanitizer.permit(:account_update, keys: attributes)
     end
-    
+
+    def after_sign_in_path_for(resource)
+        if current_user.admin?
+            admin_area_index_path
+        elsif current_user.moderator?
+            my_business_area_index_path
+        else
+            my_area_index_path
+        end
+    end
+
 end
   
